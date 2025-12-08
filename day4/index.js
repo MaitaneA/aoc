bb4c227a7957e24f417811501aed598069c2f6a3
Maia <13179021+MaitaneA@users.noreply.github.com>
2025-12-08 12:00:00 +0100

    Day 4.2

--- day4/index.js
+++ day4/index.js
@@ -18,8 +18,56 @@ async function getInput() {
 	}	
 }
 
-// Get solution (part 1)
+// Get solution (part 2)
 getInput().then(function () {
+	let keepGoing = true;
+	
+	while (keepGoing) {
+		keepGoing = false;
+		
+		for (let row = 0; row < input.length; row++) {
+			console.log(input[row]);
+			let newRow = '';
+			
+			for (let col = 0; col < input[row].length; col++) {
+				if (input[row][col] == '.') {
+					newRow += input[row][col];
+					continue;
+				}
+				
+				let adjacentCount = 0;
+				
+				for (let i = row-1; i <= row+1; i++) {
+					for (let j = col-1; j<= col+1; j++) {
+						if (i >= 0 && i < input.length && j >= 0 && j < input[row].length) {
+							if ((i != row || j != col) && input[i][j] == '@') {
+								adjacentCount++;
+							}
+						}
+					}
+				}
+				
+				if (adjacentCount < 4) {
+					accessibleRolls++;
+					newRow += '.';
+					keepGoing = true;
+				} else {
+					newRow += '@';
+				}
+			}
+			
+			input[row] = newRow;
+		}
+		
+		console.log('--------------------- Iteration end - count so far: ' + accessibleRolls);
+	}
+	
+	console.log('Solution: ' + accessibleRolls);
+
+});
+
+// Get solution (part 1)
+/*getInput().then(function () {
 	for (let row = 0; row < input.length; row++) {
 		console.log('Row ' + row);
 		for (let col = 0; col < input[row].length; col++) {
@@ -50,4 +98,4 @@ getInput().then(function () {
 
 	console.log('Solution: ' + accessibleRolls);
 
-});
+});*/
