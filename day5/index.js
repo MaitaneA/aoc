6e5af9db64a6589c42a33be01246ecc892324bd0
Maia <13179021+MaitaneA@users.noreply.github.com>
2025-12-11 00:05:24 +0100

    Still incorrect - trying to simplify

--- day5/index.js
+++ day5/index.js
@@ -1,7 +1,7 @@
 var freshIDs = 0;
 //var input = '3-5\n10-14\n16-20\n12-18\n\n1\n5\n8\n11\n17\n32';
-var input = ['3-5', '10-14', '16-20', '12-18', '15-22', '23-23', '23-23', '10-10', '14-14', '', '1', '5', '8', '11', '17', '32'];
-//var input = [];
+//var input = ['3-5', '10-14', '16-20', '12-18', '15-22', '23-23', '23-23', '10-10', '14-14', '', '1', '5', '8', '11', '17', '32'];
+var input = [];
 var freshRanges = [];
 // Only needed for part 1 -> var availableIDs = [];
 
@@ -46,44 +46,36 @@ getInput().then(function () {
 	processInput2();
 	console.log('Input has been processed & sorted');
 
-	let previousRangeSkip = -1
+	let cleanedRangeList = [];
+	
+	cleanedRangeList.push(freshRanges[0]);
 	
 	for (let i = 1; i < freshRanges.length; i++) {
-		let skipThisRange = false;
-		
+			
 		// [a,b] & [c,d]
-		let a = freshRanges[i+previousRangeSkip][0];
-		let b = freshRanges[i+previousRangeSkip][1];
+		let a = cleanedRangeList[cleanedRangeList.length - 1][0];
+		let b = cleanedRangeList[cleanedRangeList.length - 1][1];
 		let c = freshRanges[i][0];
 		let d = freshRanges[i][1];
 		
 		console.log('Range [' + a + ', ' + b + '] vs [' + c + ', ' + d + ']');
-		if (c <= b && c > a && d > b) {
+		if (c > b) {
+			cleanedRangeList.push(freshRanges[i]);
+		} else if (c <= b && c > a && d > b) {
 			console.log('Situation 2');
-			freshRanges[i+previousRangeSkip][1] = freshRanges[i][1];
-			skipThisRange = true;
+			cleanedRangeList[cleanedRangeList.length - 1][1] = freshRanges[i][1];
 		} else if (c < b && c >= a && d <= b) {
 			console.log('Situation 3');
-			skipThisRange = true;
+			continue;
 		} else if (c < b && c == a && d > b) {
 			console.log('Situation 4');
-			continue;
+			cleanedRangeList.pop();
+			cleanedRangeList.push(freshRanges[i]);
 		}
-		// if c > b no need to do anything
-		
-		
-		if (skipThisRange) {
-			if(i == freshRanges.length - 1) {
-				freshIDs += (freshRanges[i+previousRangeSkip][1] - freshRanges[i+previousRangeSkip][0] + 1);
-			}
-			
-			previousRangeSkip --;
-		} else {
-			freshIDs += (freshRanges[i+previousRangeSkip][1] - freshRanges[i+previousRangeSkip][0] + 1);
-			
-			previousRangeSkip = -1;
-			console.log('freshIDs count is now: ' + freshIDs);
-		}
+	}
+	
+	for (let i = 0; i < cleanedRangeList.length; i++) {
+		freshIDs += (cleanedRangeList[i][1] - cleanedRangeList[i][0] + 1);
 	}
 	
 	console.log('Solution: ' + freshIDs);
